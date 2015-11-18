f75a278e06f37078b86ae8103d36ff3169891c4e
Tania Cheong Cheng Ee <taniacce@me.com>
2015-11-18 17:03:37 +0800

    ver1

--- simpleServer.js
+++ simpleServer.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var server = express();
-
+var express = require('express')
+, app = express()
+, server = require('http').createServer(app);
+ 
 // Use www as the "root" directory for all requests.
 // if no path is given, it will look for index.html in that directoy.
-server.use(express.static('www'));
+app.use(express.static('www'));
 
 // Start the server listening on a port
 server.listen(3000, function(){
 	console.log ("server listening on port " + 3000);
 });
-
-
-
 
